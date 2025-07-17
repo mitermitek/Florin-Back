bb6f08eaff99aafe6bcdcb9f52142985eed4f467
mitermitek <188499468+mitermitek@users.noreply.github.com>
2025-07-17 18:48:00 +0200

    refactor: reorganize API routes for authentication and remove unused user route

--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,11 @@
 
 use App\Http\Controllers\API\V1\Auth\LoginController;
 use App\Http\Controllers\API\V1\Auth\RegisterController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::prefix('auth')->group(function () {
-    Route::post('/register', RegisterController::class);
-    Route::post('/login', LoginController::class);
+Route::prefix('v1')->group(function () {
+    Route::prefix('auth')->group(function () {
+        Route::post('register', RegisterController::class);
+        Route::post('login', LoginController::class);
+    });
 });
