3528cf4722c082f6f412815a94b26a044dd9783f
mitermitek <188499468+mitermitek@users.noreply.github.com>
2025-08-31 17:57:22 +0200

    feat: add constraints for Username and Email fields in Users table

---
+++ Data/Migrations/20250831154817_AddUsernameEmailConstraints.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Florin_Back.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUsernameEmailConstraints : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "Users",
+                type: "varchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext"
+            ).Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "varchar(254)",
+                maxLength: 254,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext"
+            ).Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "UC_Users_Username",
+                table: "Users",
+                column: "Username",
+                unique: true
+            );
+
+            migrationBuilder.CreateIndex(
+                name: "UC_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "UC_Users_Username",
+                table: "Users"
+            );
+
+            migrationBuilder.DropIndex(
+                name: "UC_Users_Email",
+                table: "Users"
+            );
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "Users",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldMaxLength: 50
+            ).Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(254)",
+                oldMaxLength: 254
+            ).Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
